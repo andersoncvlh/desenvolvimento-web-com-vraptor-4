30040b1f4fae2ea38d7e6291281f3a2af0b26110
Anderson Carvalho Martins <anderson.martins-for@MVFOR.local>
2018-06-21 17:39:33 -0300

    Adicionando postgres

--- src/main/resources/liquibase/db-changelog-1.0.sql
+++ src/main/resources/liquibase/db-changelog-1.0.sql
@@ -6,7 +6,7 @@ CREATE SEQUENCE produto_id_seq
     CACHE 1;
 
 CREATE TABLE produto (
-	id bigint PRIMARY KEY DEFAULT NEXTVAL("produto_id_seq"::regclass),
+	id bigint PRIMARY KEY DEFAULT NEXTVAL('produto_id_seq'::regclass),
 	nome character varying(255) NOT NULL,
 	valor numeric(30, 2) NOT NULL,
 	quantidade bigint
@@ -20,7 +20,7 @@ CREATE SEQUENCE usuario_id_seq
     CACHE 1;
 
 CREATE TABLE usuario (
-	id bigint PRIMARY KEY DEFAULT NEXTVAL("usuario_id_seq"::regclass),
+	id bigint PRIMARY KEY DEFAULT NEXTVAL('usuario_id_seq'::regclass),
 	nome character varying(150) NOT NULL,
-	senha character varying(50) NOT NULL,
+	senha character varying(50) NOT NULL
 );
